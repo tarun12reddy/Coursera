be125f0fe1e061ead9a7b5ff836392d6c7997d3a
tarun12reddy <tarun12reddy@gmail.com>
2016-06-15 17:06:25 -0500

    module 3 final assignment -- just started

--- RubyonRails/Course1/Module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ RubyonRails/Course1/Module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,15 @@
 require 'httparty'
 
-class Recipe
+$FOOD2FORK_KEY = '965394a15ca3712cbb3be41f584af68f'
 
+class Recipe
+	include HTTParty
+
+	base_uri 'http://food2fork.com/api/search?'
+	default_params key: $FOOD2FORK_KEY, q:  "search"
+	format :json
+
+	def self.for term
+		get("", query: {query: term})["recipes"]
+	end
 end
