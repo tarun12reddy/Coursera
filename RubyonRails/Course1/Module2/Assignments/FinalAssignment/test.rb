c5d438883250d7c304cb73971a84ef915dcb46da
tarun12reddy <tarun12reddy@gmail.com>
2016-06-15 17:06:00 -0500

    module 2 assignment final -- need correct last part

--- RubyonRails/Course1/Module2/Assignments/FinalAssignment/test.rb
+++ RubyonRails/Course1/Module2/Assignments/FinalAssignment/test.rb
@@ -1,6 +1,7 @@
 #puts Dir.pwd
 Dir.chdir 'C:\Users\Talet\Documents\SourceTree_Coursera\RubyonRails\Course1\Module2\Assignments\FinalAssignment'
 puts Dir.pwd
+require 'set'
 
 class LineAnalyzer
 	attr_reader :content, :line_number, :highest_wf_count, :highest_wf_words
@@ -57,7 +58,7 @@ class Solution
 	def print_highest_word_frequency_across_lines()
 		line_number = 1
 		@analyzers.each do |object|
-			if object.highest_wf_count == @highest_count_across_lines and object.highest_wf_words === @highest_count_words_across_lines
+			if object.highest_wf_count == @highest_count_across_lines and (object.highest_wf_words & @highest_count_words_across_lines == object.highest_wf_words)
 				puts "#{object.highest_wf_words} (appears in line #{line_number})"
 			end
 			line_number += 1
@@ -69,5 +70,3 @@ solution = Solution.new
 solution.analyze_file('test.txt')
 solution.calculate_line_with_highest_frequency
 solution.print_highest_word_frequency_across_lines()
-
-puts (2, 3, 4) == 2
